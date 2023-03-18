e33bdffb45bd8420ebca48641a0c5f2bd1584232
donura24 <teodorpenchev24@gmail.com>
2023-03-18 22:19:25 +0200

    first

--- src/main/java/com/springBootSecurity/sbs/SbsApplication.java
+++ src/main/java/com/springBootSecurity/sbs/SbsApplication.java
@@ -1,7 +1,10 @@
 package com.springBootSecurity.sbs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SbsApplication {
