a431c404f9166c81f2afe600141efdcba23f68ad
donura24 <teodorpenchev24@gmail.com>
2023-03-18 22:02:18 +0200

    first

---
+++ src/test/java/com/springBootSecurity/sbs/SbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootSecurity.sbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
