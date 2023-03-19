b68b95d491f9b48bc985f8e47138089e0e23ac9d
donura24 <teodorpenchev24@gmail.com>
2023-03-19 21:07:45 +0200

    first

--- src/main/java/com/springBootSecurity/sbs/Controller.java
+++ src/main/java/com/springBootSecurity/sbs/Controller.java
@@ -4,15 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/api/v1/greetings")
 public class Controller {
-    @GetMapping
+    @GetMapping("/hello")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hello form the API!");
     }
-    @GetMapping
+    @GetMapping("/goodbye")
     public ResponseEntity<String> sayGoodBye(){
         return ResponseEntity.ok("Good bye!");
     }
--- src/main/java/com/springBootSecurity/sbs/SbsApplication.java
+++ src/main/java/com/springBootSecurity/sbs/SbsApplication.java
@@ -1,6 +1,7 @@
 package com.springBootSecurity.sbs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,12 @@ public class SbsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbsApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+
+		};
 	}
 
 }
