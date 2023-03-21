2765e0bd5bd55fce04dce302b4bff07593abf148
donura24 <teodorpenchev24@gmail.com>
2023-03-21 19:34:14 +0200

    first

--- src/main/java/com/springBootSecurity/sbs/config/JwtAuthFilter.java
+++ src/main/java/com/springBootSecurity/sbs/config/JwtAuthFilter.java
@@ -4,6 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,8 +26,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String jwtToken;
 
         if (authHeader == null || !authHeader.startsWith("Bearer")){
-
+           filterChain.doFilter(request, response);
+           return;
         }
+        jwtToken = authHeader.substring(7);
+        userEmail = "teo@gmail.com"; // TODO
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            final boolean isTokenValid; // TODO
+
+            if (isTokenValid){
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder. getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
 
     }
 }
