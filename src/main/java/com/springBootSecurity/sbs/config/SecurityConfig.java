e25111e6da28869da3b87078158d59c54e492692
donura24 <teodorpenchev24@gmail.com>
2023-03-24 21:47:44 +0200

    first

--- src/main/java/com/springBootSecurity/sbs/config/SecurityConfig.java
+++ src/main/java/com/springBootSecurity/sbs/config/SecurityConfig.java
@@ -41,7 +41,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
+                //.anyMatchers("/**/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
